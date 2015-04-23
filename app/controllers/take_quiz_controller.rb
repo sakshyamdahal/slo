b8a0c43f0063d1b75488f3e5521e42f9588dfd98
Sakshyam Dahal <dahalsakshyam@gmail.com>
2015-04-23 06:41:09 -0400

    get users to do the quiz

--- app/controllers/take_quiz_controller.rb
+++ app/controllers/take_quiz_controller.rb
@@ -2,5 +2,19 @@ class TakeQuizController < ApplicationController
   def take
   	@quiz = Test.find(params[:quiz_id])
   	@questions = @quiz.questions
+  	@user = current_user
+  end
+
+  def evaluate
+  	@quiz = Test.find(params[:quiz_id])
+  	@score = 0
+  	@totalScore = @quiz.questions.count
+
+  	@quiz.questions.each do |q| 
+  		@optionChoosen = Option.find(params[q.id.to_s].to_i)
+  		if @optionChoosen.correct
+  			@score += 1
+  		end
+  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'take_quiz/take'
+  patch 'take_quiz/evaluate'
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :options
